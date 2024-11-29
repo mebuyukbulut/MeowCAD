1a9b15b28422971edb49b42dea13a978033576b5
muhammet esat buyukbulut <muhammetesat@riders.ai>
2024-11-29 12:18:57 +0300

    Now we have got a toolbar in viewport :)

--- MeowCAD/UI.cpp
+++ MeowCAD/UI.cpp
@@ -75,6 +75,8 @@ void UI::menu_bar() {
         ImGui::EndMainMenuBar();
     }
 
+
+
 }
 
 void UI::text_editor_window() {
@@ -215,9 +217,39 @@ void UI::viewport_window(){
         ImVec2(0, 1),
         ImVec2(1, 0)
     );
-    ImGui::End();
 
+
+    // viewport toolbar BEGIN 
+    float menuBarHeight = 25;
+    float toolbarSize = 30;
+    ImGuiWindow* viewport = ImGui::GetCurrentWindow();
+    //ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(ImVec2(viewport->Pos.x, viewport->Pos.y + menuBarHeight));
+    ImGui::SetNextWindowSize(ImVec2(viewport->Size.x, toolbarSize));
+    ImGui::SetNextWindowViewport(ImGui::GetMainViewport()->ID);
+
+    ImGuiWindowFlags window_flags = 0
+        | ImGuiWindowFlags_NoDocking
+        | ImGuiWindowFlags_NoTitleBar
+        | ImGuiWindowFlags_NoResize
+        | ImGuiWindowFlags_NoMove
+        | ImGuiWindowFlags_NoScrollbar
+        | ImGuiWindowFlags_NoSavedSettings
+        ;
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+        ImGui::Begin("TOOLBAR", NULL, window_flags);
+        ImGui::PopStyleVar();
+
+        ImGui::Button("Toolbar goes here", ImVec2(0, 37));
+
+        ImGui::End();
+    // viewport toolbar END
+
+
+    ImGui::End();
     ImGui::PopStyleVar();
+
+
 }
 
 void UI::outliner_window() {
